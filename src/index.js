c15a904b497f90fa713c6b97347a78a4be6cf803
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-10 13:05:23 -0700

    set up a bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
 
   return element;
 }
