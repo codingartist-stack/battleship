f0b0c666780e422dac5fa20c3d09561a13c29052
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-16 14:31:09 -0700

    got it working, all five battle ships created

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import _ from 'lodash';
 import './style.css';
 import { ship } from './ships.js';
 
+//SHIPS
+// Carrier = 5
+// Battleship = 4
+// Cruiser = 3
+// Submarine = 3
+// Destroyer = 2
+
 const carrier = ship('Carrier', 5);
+const battleship = ship('Battleship', 4);
+const cruiser = ship('Cruiser', 3);
+const submarine = ship('Submarine', 3);
+const destroyer = ship('Destroyer', 2);
 
 console.log(carrier);
+console.log(battleship);
+console.log(cruiser);
+console.log(submarine);
+console.log(destroyer);
