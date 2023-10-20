f858d148044f2f731f110f50d8ca87bf59a2aa4c
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-19 21:59:57 -0700

    trying to set up an test for array in array

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,1 +1,6 @@
 // create a 10 by 10 board
+const Gameboard = require('./src/gameboard');
+
+test('create board', () => {
+  expect(Gameboard(1)).toContain();
+});
--- src/gameboard.js
+++ src/gameboard.js
@@ -1,1 +1,18 @@
 //usually 10 x 10
+function Gameboard(size) {
+  let board = [];
+  const initialize = () => {
+    for (let i = 0; i < size; i++) {
+      board[i] = [];
+      //   for (let j = 0; j < array.length; j++) {
+      //     board[i][j] = ' ';
+      //   }
+    }
+  };
+
+  initialize();
+
+  return board;
+}
+
+module.exports = Gameboard;
