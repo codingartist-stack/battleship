a740475fc5ee84bf164e8bddfa68b38a4d5b7a48
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-26 12:16:47 -0700

    when using jest you have to require the other modules, not import it

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,4 +1,5 @@
 const GameBoard = require('./src/gameboard');
+const Ship = require('./src/ships');
 
 test('create board', () => {
   let testBoard = GameBoard();
@@ -39,3 +40,29 @@ test('hit target', () => {
 // let ship = new Ship(2);
 // let ships = [ship];
 // [[HIT, HIT][(MISS, null)]];
+
+test('place Ships', () => {
+  //   let carrier = Ship(5);
+  //   let battleship = Ship(4);
+  //   let cruiser = Ship(3);
+  //   let submarine = Ship(3);
+  //   let destroyer = Ship(2);
+  //   let ships = [carrier, battleship, cruiser, submarine, destroyer];
+
+  let testBoard = GameBoard();
+
+  expect(testBoard.displayBoard()).toEqual(
+    expect.arrayContaining([
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+      [[], [], [], [], [], [], [], [], [], []],
+    ])
+  );
+});
