25148b51a203bc0d9dfd6019cd14b0c90dce356f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-29 18:48:16 -0700

    writing note to myself for when I come back to place ships

--- src/gameboard.js
+++ src/gameboard.js
@@ -145,6 +145,7 @@ function GameBoard() {
   };
 
   //place ship horizontal and vertical
+  //placeShip(i, j, length, direction)
 
   const shipsSunk = () => {
     for (let i = 0; i < ships.length; i++) {
