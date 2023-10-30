fa2084ac59348bbd8f421ba4b3e395c6ff1c75b3
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-29 19:53:02 -0700

    created a function to switch turn using a boolean.

--- src/player.js
+++ src/player.js
@@ -1,7 +1,26 @@
-function Player() {
+const GameBoard = require('./src/gameboard');
+
+function Player(name) {
   //player takes turn
   //computer player attacks, can't attack same place twice.
   let playerAttacks = new Set();
+  let playerTurn = true;
+
+  let playerBoard = GameBoard();
+
+  const switchTurn = () => {
+    if (playerTurn == true) {
+      playerTurn = false;
+      return playerTurn;
+    } else {
+      playerTurn = true;
+      return playerTurn;
+    }
+  };
+
+  return {
+    switchTurn,
+  };
 }
 
 module.exports = Player;
