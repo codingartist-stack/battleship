1f83018bfbf59a1066a369fc8fadd98fba852014
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-10-31 09:52:08 -0700

    created the header and main div. started the the game function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+const gameBoard = require('./gameboard');
+const player = require('./player');
+const ships = require('./ships');
 
-//SHIPS
-// Carrier = 5
-// Battleship = 4
-// Cruiser = 3
-// Submarine = 3
-// Destroyer = 2
+const header = document.createElement('header');
+const mainDiv = document.createElement('main');
+
+function Game() {
+  //create two players
+  let person = player();
+  let computer = player();
+
+  //game loop
+
+  //how to have the players board?
+  const renderBoard = () => {};
+}
